5a982ea998da428a8cff529f2ceb991be55228ea
Urquiza <urquiza-nobrega@outlook.com>
2020-07-27 18:41:14 -0300

    added assets and project completion

--- public/scripts.js
+++ public/scripts.js
@@ -5,17 +5,31 @@ const preparationLink = document.querySelector('.show-preparation')
 const preparation = document.querySelector('.preparation')
 const ingredientsLink = document.querySelector('.show-ingredients')
 const ingredients = document.querySelector('.ingredients')
+const linkAbout = document.querySelector('#about')
+const linkRecipes = document.querySelector('#recipes')
 
 let showIngredients = true
 let showPreparation = true
 let showInformation = true
+let pathURL = window.location.pathname
+
+console.log(pathURL)
+
+if (pathURL == '/about') {
+    linkAbout.classList.add('menu-active')
+} else if (pathURL == '/') {
+    linkRecipes.classList.remove('menu-active')
+    linkAbout.classList.remove('menu-active')
+} else {
+    linkRecipes.classList.add('menu-active')
+}
 
 for (let card of cards) {
     card.addEventListener('click', function() {
         const cardId = card.getAttribute('id')
-            // modal.classList.add('active')
-        console.log('oi')
-
+        if (pathURL == `/recipe/${cardId}`) {
+            linkRecipes.classList.add('menu-active')
+        }
         window.location.href = `/recipe/${cardId}`
     })
 }
